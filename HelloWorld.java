77891faa1a12c1974a4ae4a5d4fec674aeddc5cd
aditichhabria <121998335+aditichhabria@users.noreply.github.com>
2024-03-14 10:59:14 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Thanks");
-        System.out.println("Changes");System.out.println("New changessssss");
+        System.out.println("Changes");System.out.println("modifieddddd");
          
     }
 }
